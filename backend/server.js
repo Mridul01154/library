d5de22a3d324df27d2623287450113a00e2b2c80
Mridul <toyloue47@gmail.com>
2026-05-24 22:12:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const booksRoutes = require('./routes/books');
@@ -8,6 +10,7 @@ const reviewsRoutes = require('./routes/reviews');
 const reservationsRoutes = require('./routes/reservations');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
